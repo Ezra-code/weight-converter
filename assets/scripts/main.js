4b6fd9ddc4220d97fe4b4c4ab0763138f819a367
KN-cod <ezra.njeru@student.moringaschool.com>
2022-05-23 07:55:26 +0300

    add the functionality using javascript

---
+++ assets/scripts/main.js
@@ -0,0 +1,15 @@
+let errorText = document.querySelector('#error-txt')
+let weitEl = document.querySelector('#weight')
+let convertBtn = document.querySelector('#btn')
+let converted = document.getElementById('converted')
+let weit = document.getElementById('weit')
+convertBtn.addEventListener('click', ()=> {
+    if(weitEl.value == ''){
+      errorText.style.display = 'block' 
+    }else{
+        let weight = parseInt(weitEl.value)
+        let mass = Math.round(weight * 2.2)
+        converted.style.display = 'block'
+        weit.innerHTML = mass
+    }
+})
